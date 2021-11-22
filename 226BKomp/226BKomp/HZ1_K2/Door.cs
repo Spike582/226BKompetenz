1f82a03e07ec33f2eb7bafe0343919c7e2b58781
Spike582 <61987607+Spike582@users.noreply.github.com>
2021-11-22 11:54:11 +0100

    226B Handlungsziel 1 Kompetenz 2

---
+++ 226BKomp/226BKomp/HZ1_K2/Door.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _226BKomp.HZ1_K2
+{
+    class Door
+    {
+        public int Height;
+
+        public int Width;
+
+        public string location;
+    }
+}
---
+++ 226BKomp/226BKomp/HZ1_K2/House.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _226BKomp.HZ1_K2
+{
+    class House
+    {
+        public int builddate;
+
+        public Door door { get; set; }
+
+
+        //falls man den Konstruktor von door ausführen möchte (Notiz für mich)
+        /*public House()
+        {
+            door = new Door();
+        }*/
+    }
+}
